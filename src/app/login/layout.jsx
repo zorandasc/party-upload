80b9675596cd514dbae14caaab8294c27c2c4515
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2025-07-01 09:52:04 +0200

    fixsed small

--- src/app/login/layout.jsx
+++ src/app/login/layout.jsx
@@ -7,13 +7,13 @@ const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
 
 //OVO JE WRAPPER OKO LOGIN PAGE.
 //OVO SLUZI DA AKO JE KORISNIK LOGOVAN A ODE PONOVO NA /login PAGE
-//DA GA VRATI NA "/"
+//DA GA VRATI NA "/" ILI NA "/homepage"
 //OVDIJE JE LOGIKA SLICNA KAO KOD MIDDLEWARE ALI LOGIN
 //PAGE NESMIJE BITI ZASTICEN MIDDLWAREOM
 //A ISTO TAKO POSTO LOGIN PAGE KORISTI "use client" DIRECTIVU
 //NEMOZE SE U NJEMU KORISTITI cookies().get("token");
 export default async function LoginLayout({ children }) {
-  const cookieStore = cookies();
+  const cookieStore =await cookies();
   const token = cookieStore.get("token")?.value;
 
   if (token) {
